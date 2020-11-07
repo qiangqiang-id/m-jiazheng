9e7fa2c9dfcac48ba407d8530231fdbe8a84dcb0
qiangqiang <1019805448@qq.com>
2020-11-07 21:44:03 +0800

    home结构布局

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="layout-container">
-    <van-nav-bar class="nav-header">
-      <span slot="left"
-            class="header-text">家政平台</span>
-    </van-nav-bar>
     <router-view />
     <van-tabbar v-model="active">
       <van-tabbar-item icon="home-o"
