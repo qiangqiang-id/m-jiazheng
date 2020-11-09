8f7ddbf5246249e0ba7c7f1d7c0d7ab9f06f10e2
zhangbaiming <973813763@qq.com>
2020-11-09 22:42:50 +0800

    完成入驻商务部和家政求职的基本数据传输

--- src/mock/get.js
+++ src/mock/get.js
@@ -22,6 +22,7 @@ mockjs.random = function (len, list) {
   }
   return s
 }
+
 mockjs.getMobile = function () {
   return mockjs.random(1, mockjs.mobile_prefix) +
     mockjs.random(8, mockjs.numeric)
@@ -79,6 +80,32 @@ for (let i = 0; i < 10; i++) {
 
   })
 }
+
+// 入住商务部信息
+var register = {
+  faRemVal: '个人企业', // 法人类型下拉框
+  enterprise: '龙头大亨', // 企业名称
+  displayName: '哇哈哈', // 显示名称
+  bankCard: '888888888888888888', // 社会卡号
+  showDay: '2001-10-19', // 工商注册时间
+  capital: '888', // 注册资本
+  address: '北京市北京市房山区', // 注册地区
+  enterpriseValue: '请选择', // 企业类型
+  management: ['月嫂', '育婴师', '保洁/清洗', '保姆', '早教/托', '养老/陪护', '家装/搬家', '产康师']
+}
+
+// 家政服务
+var jobWanted = {
+  name: '张三',
+  mobile: '13678987612',
+  intended: ['月嫂', '育婴师', '保洁/清洗', '保姆', '早教/托', '养老/陪护', '家装/搬家', '产康师'],
+  certificate: ['月嫂', '育婴师', '小儿推拿', '保姆', '保育员证'],
+  NativePlace: '唐朝', // 籍贯
+  CurrentAddress: '北京房区', // 现居地址
+  company: '请选择', // 意向家政公司
+  remarks: '123888' // 备注信息
+}
+
 export default {
   'http://localhost:8080/type|get': options => {
     return {
@@ -98,6 +125,36 @@ export default {
       status: 200,
       msg: '获取信息成功',
       data: companyMsg
+    }
+  },
+  'http://localhost:8080/register|get': option => { // 入住商务部
+    return {
+      status: 200,
+      msg: '获取信息成功',
+      register
+    }
+  },
+  'http://localhost:8080/register|post': options => { // 入住商务部
+    register = JSON.parse(options.body)
+    return {
+      status: 200,
+      msg: '保存信息成功',
+      register: JSON.parse(options.body)
+    }
+  },
+  'http://localhost:8080/jobWanted|get': options => { // 家政求职
+    return {
+      status: 200,
+      msg: '获取信息成功',
+      jobWanted
+    }
+  },
+  'http://localhost:8080/jobWanted|post': option => {
+    jobWanted = JSON.parse(option.body)
+    return {
+      status: 200,
+      msg: '保存成功',
+      jobWanted: JSON.parse(option.body)
     }
   }
 
--- src/views/cooperation/domestic-service.vue
+++ src/views/cooperation/domestic-service.vue
@@ -1,3 +1,4 @@
+import { Dialog } from 'vant';
 <template>
   <div class="domestic-service">
     <!-- 头部导航 -->
@@ -6,6 +7,7 @@
       left-arrow
       title=" 家政服务"
       :border='false'
+      @click-left='$router.back()'
     />
     <div class="box"></div>
     <!-- 中间表单内容 -->
@@ -16,16 +18,19 @@
       >
 
       </van-cell>
-      <van-form @submit="onSubmit">
+      <van-form
+        @submit="onSubmit"
+        autocomplete="off"
+      >
         <van-field
-          v-model="user.name"
+          v-model.trim="jobInformation.name"
           name="姓名"
           label="*姓名："
           placeholder="请输入姓名"
           :rules="userFormRules.name"
         />
         <van-field
-          v-model="user.mobile"
+          v-model="jobInformation.mobile"
           name="mobile"
           label='*手机号码：'
           placeholder="请输入手机号"
@@ -37,117 +42,54 @@
       <van-cell-group>
         <van-cell>
           <span slot="title">*意向岗位：</span>
-          <div slot="label">
+          <span
+            slot="label"
+            v-for="(item,index) in jobInformation.intended"
+            :key="index"
+          >
             <van-tag
               round
               plain
               size="medium"
-            >月嫂</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >育婴师</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >保洁员</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >保姆</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >早教/托</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >养老/陪</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >家装/搬</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >产康证</van-tag>
-
-          </div>
+            >{{item}}</van-tag>
+          </span>
         </van-cell>
       </van-cell-group>
       <van-cell-group>
         <van-cell>
           <span slot="title">*已获证书：</span>
-          <div slot="label">
+          <span
+            slot="label"
+            v-for="(item,index) in jobInformation.certificate"
+            :key="index"
+          >
             <van-tag
               round
               plain
               size="medium"
-            >月嫂</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >育婴师</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >保洁员</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >保姆</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >早教/托</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >养老/陪</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >家装/搬</van-tag>
-            <van-tag
-              round
-              plain
-              size='medium'
-            >产康证</van-tag>
-
-          </div>
+            >{{item}}</van-tag>
+          </span>
         </van-cell>
       </van-cell-group>
       <van-cell-group>
         <van-field
-          v-model="NativePlace"
+          v-model="jobInformation.NativePlace"
           label="籍贯："
         />
         <van-field
-          v-model="CurrentAddress"
+          v-model="jobInformation.CurrentAddress"
           label="现居住地："
         />
         <van-cell
-          title="意向家政公司"
+          title="意向家政公司："
           is-link
-          :value="company"
           @click="isCompanyShow=true"
-        ></van-cell>
+        >
+          <span slot="default">{{jobInformation.company}}</span>
+        </van-cell>
         <van-field
           label="备注信息："
-          v-model="remarks"
+          v-model="jobInformation.remarks"
         />
       </van-cell-group>
     </div>
@@ -155,6 +97,7 @@
     <van-button
       block
       class="btn-next"
+      @click="submitOccupation"
     >提交资料</van-button>
 
     <!-- 意向家政公司 -->
@@ -175,39 +118,72 @@
 </template>
 
 <script>
+import { Dialog } from 'vant'
 export default {
   name: 'domesticService',
   props: {},
   data () {
     return {
-      NativePlace: '', // 籍贯
-      CurrentAddress: '', // 现居地址
-      company: '请选择', // 意向家政公司
-      user: {
-        name: '',
-        mobile: ''
+      // NativePlace: '', // 籍贯
+      // CurrentAddress: '', // 现居地址
+      // company: '请选择', // 意向家政公司
+      // user: {
+      //   name: '',
+      //   mobile: ''
+      // },
+      isCompanyShow: false, // 意向家政公司弹层
+      columns: ['波力公司', '放松', '游戏', '打野'],
+      // remarks: '', // 备注信息
+      jobInformation: {
+        // name: '',
+        // mobile: '',
+        // NativePlace: '', // 籍贯
+        // CurrentAddress: '', // 现居地址
+        // company: '请选择', // 意向家政公司
+        // remarks: '' // 备注信息
       },
       userFormRules: {
         name: [{ required: true, message: '请填写用户名' }],
         mobile: [{ required: true, message: '手机号不能为空' },
           { pattern: /^1[3|5|7|8]\d{9}$/, message: '手机格式错误' }]
-      },
-      isCompanyShow: false, // 意向家政公司弹层
-      columns: ['波力公司', '放松', '游戏', '打野'],
-      remarks: '' // 备注信息
-
+      }
     }
   },
-  components: {
-
+  components: {},
+  created () {
+    this.housekeepingProfession()
   },
   methods: {
     onSubmit (values) {
       console.log('submit', values)
     },
     isInterestedCompanyShow (value) {
-      this.company = value
+      this.jobInformation.company = value
       this.isCompanyShow = false
+    },
+    async housekeepingProfession () {
+      try {
+        const { data: res } = await this.$axios.get('http://localhost:8080/jobWanted')
+        this.jobInformation = res.jobWanted
+        console.log(this.jobInformation)
+      } catch (error) {
+        this.$toast.fail('获取信息失败')
+      }
+    },
+    // 提交
+    submitOccupation () {
+      Dialog.confirm({
+        title: '提示',
+        message: '已确认信息无误'
+      }).then(async () => {
+        const { data: res } = await this.$axios.post('http://localhost:8080/jobWanted', this.jobInformation)
+        this.jobInformation = res.jobWanted
+        this.housekeepingProfession()
+        console.log('保存后', this.jobInformation)
+      }).catch(() => {
+        this.jobInformation = ''
+        this.$toast('已取消')
+      })
     }
   }
 
@@ -251,10 +227,10 @@ export default {
     transform: rotate(90deg);
     font-weight: 900;
     margin: unset;
-    color:#3F51B5;
+    color: #3f51b5;
   }
   .van-tag {
-    margin: 20px;
+    margin: 15px;
   }
   .btn-next {
     position: fixed;
@@ -263,6 +239,9 @@ export default {
     bottom: 0;
     color: #fff;
     background-color: #3f51b5;
+  }
+  .van-field__control {
+    background-color: unset;
   }
 }
 </style>
